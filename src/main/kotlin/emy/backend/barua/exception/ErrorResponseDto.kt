388182411d5667244d2a990942d2c078f21d009e
ElieOko <elieoko100@gmail.com>
2026-03-25 08:12:11 +0100

    feat:config exception

---
+++ src/main/kotlin/emy/backend/barua/exception/ErrorResponseDto.kt
@@ -0,0 +1,9 @@
+package emy.backend.barua.exception
+
+import java.time.LocalDateTime
+
+data class ErrorResponseDto(
+    val message : String,
+    val detailMessage : String,
+    val errorTime : LocalDateTime
+)
