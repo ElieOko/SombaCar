71dd060bc770fd63c214a93cd1cb80b7ee082570
ElieOko <elieoko100@gmail.com>
2026-03-25 08:10:08 +0100

    fix:add configuration de base

---
+++ src/main/kotlin/emy/backend/barua/app/user/domain/models/request/UserPassword.kt
@@ -0,0 +1,18 @@
+package server.web.casa.app.user.domain.model.request
+
+import jakarta.validation.constraints.*
+
+data class UserPassword(
+    @NotNull
+    @field:NotBlank(message = "Le nouveau mot de passe est obligatoire")
+    @field:Size(min = 6, message = "Le mot de passe doit contenir au moins 6 caractères")
+    val newPassword : String,
+    @NotNull
+    val userId : Long
+
+)
+data class CertificationState(
+    @NotNull
+    val state : Boolean = true,
+    val message : String?= null,
+)
