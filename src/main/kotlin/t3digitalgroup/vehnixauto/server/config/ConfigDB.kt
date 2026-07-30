841a94fc5e48228f648354406c010575f0a3616c
ElieOko <elieoko100@gmail.com>
2026-07-30 10:10:24 +0100

    fix:restruration files

--- src/main/kotlin/t3digitalgroup/vehnixauto/server/config/ConfigDB.kt
+++ src/main/kotlin/t3digitalgroup/vehnixauto/server/config/ConfigDB.kt
@@ -16,31 +16,31 @@ import org.springframework.util.StringUtils
 )
 class DatabaseConfig {
 
-    @Bean(initMethod = "migrate")
-    @Profile("dev")
-    fun flyway(
-        flywayProperties: FlywayProperties,
-        r2dbcProperties: R2dbcProperties,
-    ): Flyway {
-        val jdbcUrl = resolveJdbcUrl(flywayProperties, r2dbcProperties)
-        return Flyway.configure()
-            .dataSource(
-                jdbcUrl,
-                flywayProperties.user ?: r2dbcProperties.username,
-                flywayProperties.password ?: r2dbcProperties.password,
-            )
-            .locations(*flywayProperties.locations.toTypedArray())
-            .baselineOnMigrate(flywayProperties.isBaselineOnMigrate)
-            .load()
-    }
-
-    private fun resolveJdbcUrl(
-        flywayProperties: FlywayProperties,
-        r2dbcProperties: R2dbcProperties,
-    ): String {
-        flywayProperties.url?.takeIf { StringUtils.hasText(it) }?.let { return it }
-        val r2dbcUrl = r2dbcProperties.url
-            ?: throw IllegalStateException("Configure spring.flyway.url or spring.r2dbc.url for migrations.")
-        return r2dbcUrl.replace("r2dbc:", "jdbc:")
-    }
+//    @Bean(initMethod = "migrate")
+//    @Profile("dev")
+//    fun flyway(
+//        flywayProperties: FlywayProperties,
+//        r2dbcProperties: R2dbcProperties,
+//    ): Flyway {
+//        val jdbcUrl = resolveJdbcUrl(flywayProperties, r2dbcProperties)
+//        return Flyway.configure()
+//            .dataSource(
+//                jdbcUrl,
+//                flywayProperties.user ?: r2dbcProperties.username,
+//                flywayProperties.password ?: r2dbcProperties.password,
+//            )
+//            .locations(*flywayProperties.locations.toTypedArray())
+//            .baselineOnMigrate(flywayProperties.isBaselineOnMigrate)
+//            .load()
+//    }
+//
+//    private fun resolveJdbcUrl(
+//        flywayProperties: FlywayProperties,
+//        r2dbcProperties: R2dbcProperties,
+//    ): String {
+//        flywayProperties.url?.takeIf { StringUtils.hasText(it) }?.let { return it }
+//        val r2dbcUrl = r2dbcProperties.url
+//            ?: throw IllegalStateException("Configure spring.flyway.url or spring.r2dbc.url for migrations.")
+//        return r2dbcUrl.replace("r2dbc:", "jdbc:")
+//    }
 }
